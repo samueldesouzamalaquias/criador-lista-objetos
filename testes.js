9376982b1656eb7b79c2ae75aa6d7097d5890b22
samueldesouzamalaquias <138811455+samueldesouzamalaquias@users.noreply.github.com>
2023-09-23 12:22:45 -0300

    Update testes.js
    New functions added

--- testes.js
+++ testes.js
@@ -1,5 +1,6 @@
 //variáveis
 const mainDiv = document.querySelector('#main-div');
+const totParag = document.querySelector('#totParag');
 let inputNome = document.querySelector('#inputNome');
 let inputQuant = document.querySelector('#inputQuant');
 let inputPreço = document.querySelector('#inputPreço');
@@ -8,8 +9,8 @@ const btLimpar = document.querySelector('#btLimpar');
 let listaObj = [];
 
 //style
-document.querySelectorAll('input').forEach(input => input.style.marginBottom = '20px');
-document.querySelectorAll('button').forEach(button => button.style.marginBottom = '10px');
+document.querySelectorAll('input').forEach(input => input.style.marginBottom = '10px');
+document.querySelectorAll('button').forEach(button => button.style.marginBottom = '20px');
 inputQuant.style.width = '80px';
 inputPreço.style.width = '80px';
 
@@ -25,6 +26,7 @@ mainDiv.style.backgroundColor = 'orange';
 btAdicionar.addEventListener('click',function(){
     criaListaObj();
     renderListaObj();
+    updateTot();
     saveListaObj();
 });
 
@@ -32,6 +34,7 @@ btLimpar.addEventListener('click', function(){
     mainDiv.innerHTML = '';
     listaObj = [];
     localStorage.clear();
+    totParag.textContent = '';
 })
 
 //acessando localStorage
@@ -45,8 +48,9 @@ renderListaObj();
 function criaListaObj(){
     let novoObj = {};
     novoObj.nome = inputNome.value;
-    novoObj.quant = inputQuant.value;
-    novoObj.preço = inputPreço.value;
+    novoObj.quant = parseInt(inputQuant.value);
+    novoObj.preço = parseFloat(inputPreço.value);
+    novoObj.preçoTot = novoObj.quant * novoObj.preço;
     listaObj.push(novoObj);
     inputNome.value = '';
     inputQuant.value = '';
@@ -59,10 +63,11 @@ function renderListaObj(){
     for(let i=0; i<listaObj.length; i++){
     let novaDiv = `<div>${i+1}° ITEM - Nome: ${listaObj[i].nome}<br>
         Quant: ${listaObj[i].quant}<br>
-        Preço: R$${listaObj[i].preço}<br>
-        <button style="background-color: yellow "onclick="
+        Preço un: R$${listaObj[i].preço} - Preço tot: R$${listaObj[i].preçoTot}<br>
+        <button style="background-color: yellow"onclick="
           listaObj.splice(${i}, 1);
           mainDiv.innerHTML = '';
+          updateTot();
           renderListaObj();
           saveListaObj();
         ">remover</button>
@@ -76,6 +81,16 @@ function removeItem(index){
     renderListaObj();
 }
 
+function updateTot(){
+    let soma = 0;
+    for (obj of listaObj){
+        soma+= parseFloat(obj.preçoTot);
+        totParag.textContent = `Valor total dos itens: R$${soma}`;
+    }
+}
+
 function saveListaObj(){
     localStorage.setItem('listaObj', JSON.stringify(listaObj));
 }
+
+updateTot();
