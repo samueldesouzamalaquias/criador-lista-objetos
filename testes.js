04874b7fe983c374ffc126b3bcfcc53cf638d1f7
samueldesouzamalaquias <138811455+samueldesouzamalaquias@users.noreply.github.com>
2023-11-02 22:14:40 -0300

    Update testes.js
    Added functions

--- testes.js
+++ testes.js
@@ -1,12 +1,20 @@
 //variáveis
 const mainDiv = document.querySelector('#main-div');
 const totParag = document.querySelector('#totParag');
+const objDiv = document.querySelector('#objDiv');
 let inputNome = document.querySelector('#inputNome');
 let inputQuant = document.querySelector('#inputQuant');
 let inputPreço = document.querySelector('#inputPreço');
+let inputSave = document.querySelector('#inputSave');
 const btAdicionar = document.querySelector('#btAdicionar');
 const btLimpar = document.querySelector('#btLimpar');
 const btOrganizar = document.querySelector('#btOrganizar');
+const btReset = document.querySelector('#btReset');
+const btSalvar = document.querySelector('#btSalvar');
+const btAbrir = document.querySelector('#btAbrir');
+const btExcluir = document.querySelector('#btExcluir');
+const btObj = document.querySelector('#btObj');
+
 let listaObj = [];
 
 //style
@@ -19,7 +27,7 @@ inputPreço.style.width = '55px';
 buttons.forEach(function(button){
     button.style.width = '90px'
     button.style.height = '30px'
-    button.style.marginRight = '30px'
+    button.style.marginRight = '5px'
 })
 
 btAdicionar.style.backgroundColor = 'green';
@@ -51,6 +59,57 @@ function(){
     organizarListaObj()
 })
 
+btReset.addEventListener('click',
+function(){
+    listaObj = [
+    {"nome":"Absorvente","peso":"","quant":3,"preço":0,"preçoTot":0},{"nome":"Adoçante","peso":"","quant":2,"preço":0,"preçoTot":0},{"nome":"Alface","peso":"","quant":1,"preço":0,"preçoTot":0},{"nome":"Café","peso":"","quant":5,"preço":0,"preçoTot":0},{"nome":"Carne moída","peso":"500 g","quant":0,"preço":0,"preçoTot":0},{"nome":"Cheirinho para vaso sanitário","peso":"","quant":1,"preço":0,"preçoTot":0},{"nome":"Chia","peso":"","quant":0,"preço":0,"preçoTot":0},{"nome":"Creme de leite","peso":"","quant":2,"preço":0,"preçoTot":0},{"nome":"Cuzcuz Da Terrinha","peso":"","quant":1,"preço":0,"preçoTot":0},{"nome":"Desinfetante","peso":"Não informado","quant":1,"preço":0,"preçoTot":0},{"nome":"Desodorante feminino","peso":"","quant":2,"preço":0,"preçoTot":0},{"nome":"Desodorante masculino","peso":"","quant":1,"preço":0,"preçoTot":0},{"nome":"Detergente","peso":"","quant":2,"preço":0,"preçoTot":0},{"nome":"Detergente","peso":"Não informado","quant":1,"preço":0,"preçoTot":0},{"nome":"Escova de dentes","quant":3,"preço":0,"preçoTot":0},{"nome":"Filé de frango Sassami","peso":"","quant":3,"preço":0,"preçoTot":0},{"nome":"Fio dental","peso":"","quant":1,"preço":0,"preçoTot":0},{"nome":"Frutas","peso":"","quant":0,"preço":0,"preçoTot":0},{"nome":"Iogurte natural desnatado","peso":"","quant":4,"preço":0,"preçoTot":0},{"nome":"Leite","peso":"3 litros","quant":3,"preço":0,"preçoTot":0},{"nome":"Leite em pó","peso":"","quant":1,"preço":0,"preçoTot":0},{"nome":"Manteiga","peso":"","quant":2,"preço":0,"preçoTot":0},{"nome":"Luva para limpeza","peso":"","quant":1,"preço":0,"preçoTot":0},{"nome":"Mussarela","peso":"500g","quant":1,"preço":0,"preçoTot":0},{"nome":"Orégano","peso":"","quant":1,"preço":0,"preçoTot":0},{"nome":"Papel higiênico","peso":"2 grandes","quant":2,"preço":0,"preçoTot":0},{"nome":"Pasta de dentes","peso":"","quant":3,"preço":0,"preçoTot":0},{"nome":"Páprica doce","peso":"","quant":2,"preço":0,"preçoTot":0},{"nome":"Requeijão Tirolez Light","peso":"","quant":3,"preço":0,"preçoTot":0},{"nome":"Sabonete","peso":"","quant":5,"preço":0,"preçoTot":0},{"nome":"Sabão em pó","peso":"","quant":1,"preço":0,"preçoTot":0},{"nome":"Tapioca","peso":"","quant":1,"preço":0,"preçoTot":0},{"nome":"Tomate","peso":"","quant":4,"preço":0,"preçoTot":0}
+
+];
+    for(obj of listaObj){
+        obj.preçoTot = obj.quant * obj.preço;
+    }
+    renderListaObj();
+    updateTot();
+    saveListaObj();
+})
+
+btSalvar.addEventListener('click',
+function(){
+    localStorage.setItem(`${inputSave.value}`,JSON.stringify(listaObj));
+    inputSave.value = '';
+});
+
+btAbrir.addEventListener('click',
+function(){
+    if(localStorage.getItem(`${inputSave.value}`)){
+        listaObj = JSON.parse(localStorage.getItem(`${inputSave.value}`));
+        inputSave.value = '';
+        renderListaObj();
+        saveListaObj();
+    }else{
+        alert(`Lista "${inputSave.value}" não encontrada`);
+        inputSave.value = '';
+        listaObj = [];
+        renderlistaObj();
+    }
+})
+
+btExcluir.addEventListener('click',
+function(){
+    if(localStorage.getItem(`${inputSave.value}`)){
+        localStorage.removeItem(`${inputSave.value}`);
+        alert(`Lista ${inputSave.value} excluída`)
+        inputSave.value = '';
+    }else{
+        alert(`Lista "${inputSave.value}" não encontrada`)
+        inputSave.value = '';
+    }
+})
+
+btObj.addEventListener('click',
+function(){
+    objDiv.textContent = (JSON.stringify(listaObj));
+})
 
 //acessando localStorage
 if(localStorage.getItem('listaObj')){
@@ -148,8 +207,9 @@ function updateTot(){
     let soma = 0;
     for (obj of listaObj){
         soma+= parseFloat(obj.preçoTot);
-        totParag.textContent = `Valor total dos itens: R$${soma}`;
     }
+    let somaFormatada = soma.toFixed(2);
+    totParag.textContent = `Valor total dos itens: R$${somaFormatada}`;
 }
 
 function saveListaObj(){
@@ -160,6 +220,7 @@ function clearInput(){
     inputNome.value = '';
     inputQuant.value = '';
     inputPreço.value = '';
+    inputPeso.value = '';
 }
 
 function organizarListaObj(){
